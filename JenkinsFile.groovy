a151d6472fb894702c704056af5d3b2a4db29025
mgara07 <malek.gara@esprit.tn>
2022-02-18 20:04:05 +0100

    update

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -39,6 +39,38 @@ pipeline {
             }
 
         }
+        stage('Building image') {
+
+        steps {
+
+          script {
+
+            dockerImage = docker.build registry + ":$BUILD_NUMBER"
+
+          }
+
+        }
+
+      }
+
+    
+    stage('Deploy image') {
+
+      steps {
+
+        script {
+
+          docker.withRegistry('', registryCredential) {
+
+            dockerImage.push()
+
+          }
+
+        }
+
+      }
+
+    }
     }
     post { 
         always {            
