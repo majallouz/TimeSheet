c54e7d8019e85019dab98dd4a4bdbdb060e43ebc
chmayer <cyrine.hmayer@esprit.tn>
2022-02-19 00:10:38 +0100

    jenkinsFile update

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,51 +1,109 @@
 pipeline {
+  agent any
+  environment {
 
+    mvnHome = tool 'maven_3.3.3'
+    registry = "cyrinehm/timesheet"
+    registryCredential = 'dockerHub'
+    dockerImage = ''
+    
 
-    agent any
-    stages {
-         stage('clone and clean repo') {
-            steps {
-                git changelog: false, branch: 'Malek_branch',  credentialsId: 'mgara07', poll: false, url: 'https://github.com/majallouz/TimeSheet.git'
-                /* bat 'git clone https://github.com/majallouz/TimeSheet.git'
-                bat 'mvn clean -f TimeSheet' */
-            }
-        }
-        stage('Test') {
-            steps { 
-                bat 'mvn test'
-                /* bat 'mvn test -f TimeSheet' */
-            }
-            post {
+  }
+  stages {
+
+    stage('clone and clean repo') {
+      steps {
+
+        git changelog: false, branch: 'cyrine_branch', credentialsId: 'Cyrine-hm', poll: false, url: 'https://github.com/majallouz/TimeSheet.git'
+        /* bat 'git clone https://github.com/majallouz/TimeSheet.git'*/
+        bat "${mvnHome}/bin/mvn clean" 
+      }
+
+    }
+    stage('Test') {
+
+      steps {
+        bat "${mvnHome}/bin/mvn test"
+      }
+      post {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                 }
             }
-        }
-        stage('Sonar') {
-            steps {
-                bat 'mvn sonar:sonar'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                bat 'mvn package deploy '
-            }
-        }
-        
-        stage('clean ws') {
-
-            steps {
-                    cleanWs()
-            }
-
-        }
     }
-    post { 
-        always {            
-            emailext body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
-                        recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
-                        subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}",
-                        to: '$DEFAULT_RECIPIENTS'
+    stage('sonar'){
+    steps {
+    bat "mvn sonar:sonar "
+    }}
+    stage('Deploy') {
+      steps {
+
+        bat "${mvnHome}/bin/mvn package"
+        bat "${mvnHome}/bin/mvn deploy"
+
+      }
+    }
+
+       stage('Building image') {
+
+        steps {
+
+          script {
+
+            dockerImage = docker.build registry + ":$BUILD_NUMBER"
+
+          }
+
         }
-    } 
+
+      }
+
+    
+    stage('Deploy image') {
+
+      steps {
+
+        script {
+
+          docker.withRegistry('', registryCredential) {
+
+            dockerImage.push()
+
+          }
+
+        }
+
+      }
+
+    }
+    stage('Cleaning up') { 
+
+            steps { 
+
+                bat "docker rmi $registry:$BUILD_NUMBER" 
+
+            }
+
+        }
+
+   
+
+    stage('clean workspace') {
+
+      steps {
+        cleanWs()
+      }
+
+    }
+  }
+   post { 
+          always {            
+              deleteDir()
+              emailext body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
+                          recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
+                          subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}",
+                          to: '$DEFAULT_RECIPIENTS'
+          }
+      }
+  
 }
