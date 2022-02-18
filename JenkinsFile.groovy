c54675a822aa0e56f693f39ea1780188f486ca21
amira <amira.besbes@esprit.tn>
2022-02-18 23:59:10 +0100

    Update pom

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -1,51 +1,105 @@
 pipeline {
+agent any
+environment {
+
+ mvnHome = tool 'apache-maven-3.3.3'
+registry = "amirabes/timesheet"
+registryCredential = 'dockerHub'
+dockerImage = ''
+
+ }
+stages {
+
+ stage('clone and clean repo') {
+steps {
+
+ git changelog: false, branch: 'amira-branch', credentialsId: 'amira.besbes@esprit.tn', poll: false, url: 'https://github.com/majallouz/TimeSheet.git'
+/* bat 'git clone https://github.com/majallouz/TimeSheet.git'*/
+bat "${mvnHome}/bin/mvn clean"
+}
+
+ }
+stage('Test') {
+
+ steps {
+bat "${mvnHome}/bin/mvn test"
+}
+post {
+always {
+junit '**/target/surefire-reports/TEST-*.xml'
+}
+}
+}
+stage('sonar'){
+steps {
+bat "mvn sonar:sonar"
+}}
+stage('Deploy') {
+steps {
+
+ bat "${mvnHome}/bin/mvn package"
+bat "${mvnHome}/bin/mvn deploy"
+
+ }
+}
+
+ stage('Building image') {
+
+ steps {
+
+ script {
+
+ dockerImage = docker.build registry + ":$BUILD_NUMBER"
+
+ }
+
+ }
+
+ }
+
+stage('Deploy image') {
+
+ steps {
+
+ script {
+
+ docker.withRegistry('', registryCredential) {
+
+ dockerImage.push()
+
+ }
+
+ }
+
+ }
+
+ }
+stage('Cleaning up') {
+
+ steps {
+
+ bat "docker rmi $registry:$BUILD_NUMBER"
+
+ }
+
+ }
 
 
-    agent any
-    stages {
-         stage('clone and clean repo') {
-            steps {
-                git changelog: false, branch: 'Malek_branch',  credentialsId: 'mgara07', poll: false, url: 'https://github.com/majallouz/TimeSheet.git'
-                /* bat 'git clone https://github.com/majallouz/TimeSheet.git'
-                bat 'mvn clean -f TimeSheet' */
-            }
-        }
-        stage('Test') {
-            steps { 
-                bat 'mvn test'
-                /* bat 'mvn test -f TimeSheet' */
-            }
-            post {
-                always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                }
-            }
-        }
-        stage('Sonar') {
-            steps {
-                bat 'mvn sonar:sonar'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                bat 'mvn package deploy '
-            }
-        }
-        
-        stage('clean ws') {
+ stage('clean workspace') {
 
-            steps {
-                    cleanWs()
-            }
+ steps {
+cleanWs()
+}
 
-        }
-    }
-    post { 
-        always {            
-            emailext body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
-                        recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
-                        subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}",
-                        to: '$DEFAULT_RECIPIENTS'
-        }
-    } 
+ }
+}
+post {
+always {
+deleteDir()
+emailext body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
+recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
+subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}",
+to: '$DEFAULT_RECIPIENTS'
+}
+}
 }
